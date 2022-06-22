04e43ebbc2366f3f0d94afe60c2e3b9772406a31
uctina <105360091+uctina@users.noreply.github.com>
2022-06-22 12:47:45 +0100

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,5 @@
+var name, height, country
+name=prompt("Enter your name")
+height=prompt("Enter your height")
+country=prompt("Enter your country")
+document.write(name+ "<br>"+height+ "<br>"+country)
